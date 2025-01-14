45fa8a5d39c4bb911434e381ce1182cb25873cf1
Adam <adamomarali37@gmail.com>
2025-01-13 20:40:12 -0500

    update env var name

--- firebase/clientConfig.ts
+++ firebase/clientConfig.ts
@@ -15,7 +15,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_SECRET,
   authDomain: "feynman-361723.firebaseapp.com",
   projectId: "feynman-361723",
   storageBucket: "feynman-361723.appspot.com",
